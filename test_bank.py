8f9fe0c737581b4969183bd497d918b79cef5bdd
John_2563 <102374100+JohnZolton@users.noreply.github.com>
2022-08-13 10:15:53 -0400

    Delete test_bank.py

--- test_bank.py
+++
@@ -1,9 +0,0 @@
-from bank import value
-
-def test_bank():
-    assert value('sup') == 100
-    assert value('hello') == 0
-    assert value('hi') == 20
-    assert value('Sup') == 100
-    assert value('Hello') == 0
-    assert value('Hi') == 20
