15b2d4fbaed5c9a90d64a8dd8da70a6129d525b4
John_2563 <102374100+JohnZolton@users.noreply.github.com>
2022-08-13 10:16:07 -0400

    Delete test_numb3rs.py

--- test_numb3rs.py
+++
@@ -1,9 +0,0 @@
-from numb3rs import validate
-
-def test_good():
-    assert validate('1.1.1.1') == True
-
-def test_bad():
-    assert validate('1.2.2.256')== False
-    assert validate('a.b.b.c') == False
-    assert validate('cat') == False
