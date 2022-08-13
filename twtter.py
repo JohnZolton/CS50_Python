29e3d8334ef94a08806ed6105a3cca3f5c9f2485
John_2563 <102374100+JohnZolton@users.noreply.github.com>
2022-08-13 10:16:39 -0400

    Delete twtter.py

--- twtter.py
+++
@@ -1,8 +0,0 @@
-text =  input('enter your tweet: ').strip()
-
-for i in text:
-    if i.lower()== 'a' or i.lower()=='e' or i.lower()=='i' or i.lower()=='o' or i.lower()== 'u':
-        print('', sep='', end ='')
-    else:
-        print(i, sep= '', end='')
-print()
