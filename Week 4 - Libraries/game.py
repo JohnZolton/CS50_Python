51c16096a4224e1058d582381586a0dc685da2aa
John_2563 <102374100+JohnZolton@users.noreply.github.com>
2022-08-13 10:10:51 -0400

    Add files via upload

---
+++ Week 4 - Libraries/game.py
@@ -0,0 +1,28 @@
+import random
+
+while True:
+    try:
+        level = input('Level: ')
+        if int(level) > 0:
+            break
+    except ValueError:
+        continue
+
+level = int(level)
+answer = random.randrange(0, level)
+
+while True:
+    guess = input('Guess: ')
+    try:
+        if int(guess) >0 and int(guess) <= level:
+            guess = int(guess)
+            
+            if guess == answer:
+                print('Just right!')
+                break
+            elif guess > answer:
+                print('Too large!')
+            elif guess < answer:
+                print('Too small!')
+    except ValueError:
+        continue
